4f1dd26e0a62d9173220ee370b9478225339feca
Mirco Mohr <mirco.entw@gmail.com>
2021-01-17 01:11:25 +0100

    Init

---
+++ app.js
@@ -0,0 +1,25 @@
+const { resolve } = require("dns");
+const express = require("express");
+var fileupload = require("express-fileupload");
+const app = express();
+
+app.use(fileupload());
+const port = 8080;
+
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/ocr", (req, res) => {
+  const { spawn } = require("child_process");
+
+  const child = spawn("tesseract", ["stdin", "stdout"]);
+  child.stdout.on("data", (data) => res.send(data));
+  child.stdout.on("error", console.log);
+  child.stdin.write(req.files.img.data);
+  child.stdin.end();
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
